3a58486c1dabd2bb1aa0f0113f1337ea3364dbd2
CTelloMeli <carlos.telloruiz@mercadolibre.com.co>
2022-10-10 19:48:07 -0500

    test3G26

--- script.js
+++ script.js
@@ -42,32 +42,42 @@ function createCategory(data) {
 //Load data
 function loadData(){
     const table = document.getElementById("categoryList").getElementsByTagName('tbody')[0];
-    $.ajax({
-        url : `http://129.158.60.253:8080/api/Category/all`,
-        data : null,
-        type : "GET", //POST, PUT, DELETE, GET
-        dataType : 'json',
-        success: function(data) {
-            data.items.map(item => {
-                const newRow = table.insertRow();
-                cell1 = newRow.insertCell(0);
-                    cell1.innerHTML = item.id;
-                cell2 = newRow.insertCell(1);
-                    cell2.innerHTML = item.name;
-                cell3 = newRow.insertCell(2);
-                    cell3.innerHTML = item.description;
-                cell4 = newRow.insertCell(4);
-                    cell4.innerHTML = `<button onClick="onSelect(this)">Select</button> <button onClick="onDelete(this,${item.id})">Delete</button>`;
-            })
-        },
-        error: function(error) {
-            alert('Error');
-            console.log('errorLoad -->', error);
-        },
-        complete : function(xhr, status) {
-            console.log('load OK');
+    fetch('http://129.158.60.253:8080/api/Category/all', {
+        method: 'GET', // or 'PUT'
+        // body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
         }
-    })
+      }).then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', response));
+
+    // $.ajax({
+    //     url : `http://129.158.60.253:8080/api/Category/all`,
+    //     data : null,
+    //     type : "GET", //POST, PUT, DELETE, GET
+    //     dataType : 'json',
+    //     success: function(data) {
+    //         data.items.map(item => {
+    //             const newRow = table.insertRow();
+    //             cell1 = newRow.insertCell(0);
+    //                 cell1.innerHTML = item.id;
+    //             cell2 = newRow.insertCell(1);
+    //                 cell2.innerHTML = item.name;
+    //             cell3 = newRow.insertCell(2);
+    //                 cell3.innerHTML = item.description;
+    //             cell4 = newRow.insertCell(4);
+    //                 cell4.innerHTML = `<button onClick="onSelect(this)">Select</button> <button onClick="onDelete(this,${item.id})">Delete</button>`;
+    //         })
+    //     },
+    //     error: function(error) {
+    //         alert('Error');
+    //         console.log('errorLoad -->', error);
+    //     },
+    //     complete : function(xhr, status) {
+    //         console.log('load OK');
+    //     }
+    // })
     
 }
 loadData();
