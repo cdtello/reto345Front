1a220e055e5eefe3900ef33989ae8cae67410259
CTelloMeli <carlos.telloruiz@mercadolibre.com.co>
2022-10-11 22:00:36 -0500

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var selectedRow = null
 
-
+const host = '129.158.60.253';
+//const host = 'localhost';
 
 //*******    CRUD  CATEGORY     *******/ 
 
@@ -14,15 +15,23 @@ function onCategorySubmit(e) {
 //Retrieve the data
 function readFormCategoryData() {
     var formData = {};
+    formData["id"] = document.getElementById("categoryId").value;
     formData["name"] = document.getElementById("categoryName").value;
     formData["description"] = document.getElementById("categoryDescription").value;
     return formData;
 }
 
-function createCategory(data) {
+function createCategory(data){
+    const url = `http://${host}:8080/api/Category/save`;
+
     $.ajax({
-        url : `http://129.158.60.253:8080/api/Category/save`,
-        data : data,
+        url : url,
+        data : JSON.stringify(data),
+        headers: { 
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': true 
+        },
         type : "POST", //POST, PUT, DELETE
         dataType : 'json',
         success: function() {
@@ -32,106 +41,98 @@ function createCategory(data) {
             console.log('errorInsert -->', error);
         },
         complete : function(xhr, status) {
-            //location.reload();
-            loadData();
+            location.reload();
         }
     })
 }
-
-
 //Load data
-function loadData(){
+function loadCategoryData(){
     const table = document.getElementById("categoryList").getElementsByTagName('tbody')[0];
-    fetch('http://129.158.60.253:8080/api/Category/all', {
-        method: 'GET', // or 'PUT'
-        // body: JSON.stringify(data), // data can be `string` or {object}!
-        headers:{
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json())
-      .catch(error => console.error('Error:', error))
-      .then(response => console.log('Success:', response));
 
-    // $.ajax({
-    //     url : `http://129.158.60.253:8080/api/Category/all`,
-    //     data : null,
-    //     type : "GET", //POST, PUT, DELETE, GET
-    //     dataType : 'json',
-    //     success: function(data) {
-    //         data.items.map(item => {
-    //             const newRow = table.insertRow();
-    //             cell1 = newRow.insertCell(0);
-    //                 cell1.innerHTML = item.id;
-    //             cell2 = newRow.insertCell(1);
-    //                 cell2.innerHTML = item.name;
-    //             cell3 = newRow.insertCell(2);
-    //                 cell3.innerHTML = item.description;
-    //             cell4 = newRow.insertCell(4);
-    //                 cell4.innerHTML = `<button onClick="onSelect(this)">Select</button> <button onClick="onDelete(this,${item.id})">Delete</button>`;
-    //         })
-    //     },
-    //     error: function(error) {
-    //         alert('Error');
-    //         console.log('errorLoad -->', error);
-    //     },
-    //     complete : function(xhr, status) {
-    //         console.log('load OK');
-    //     }
-    // })
-    
+    $.ajax({
+        url : `http://${host}:8080/api/Category/all`,
+        data : null,
+        headers: {  
+            'Access-Control-Allow-Origin': true
+        },
+        type : "GET", //POST, PUT, DELETE, GET
+        dataType : 'json',
+        success: function(data) {
+            console.log('data -->', data)
+            data.map(item => {
+                const newRow = table.insertRow();
+                cell1 = newRow.insertCell(0);
+                    cell1.innerHTML = item.id;
+                cell2 = newRow.insertCell(1);
+                    cell2.innerHTML = item.name;
+                cell3 = newRow.insertCell(2);
+                    cell3.innerHTML = item.description;
+                cell4 = newRow.insertCell(3);
+                cell4.innerHTML = `<button onClick="categorySelect(this)">Select</button> <button onClick="categoryDelete(this,${item.id})">Delete</button>`;
+                    
+            })
+        },
+        error: function(error) {
+            alert('Error');
+            console.log('errorLoad -->', error);
+        },
+        complete : function(xhr, status) {
+            console.log('load OK');
+        }
+    }) 
 }
-loadData();
+
+loadCategoryData();
 //Insert the data
 
 
 //Edit the data
-function onSelect(td) {
+function categorySelect(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("product").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("categoryId").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("categoryName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("categoryDescription").value = selectedRow.cells[2].innerHTML;
 }
-function updateRecord() {
-     fetch(url, {
-        method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-    }).then(data => data.json())
-    .then(response => renderPokemonData(response))
-    .catch(err => renderNotFound())
-
-
-    const formData = readFormData();
+function categoryUpdate() {
+    const url = `http://${host}:8080/api/Category/update`;
+    const formData = readFormCategoryData();
     console.log('formData ->', formData)
-    const data = {nombre: formData.nombre, apellido: formData.apellido, edad: formData.edad}
+    const data = {name: formData.name, description: formData.description, id: formData.id}
     $.ajax({
-        url : `https://g21b567102d46a9-f8e2c8h6ah6jkxtz.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/estudiantes/estudiantes/${formData.id}`,
-        data : data,
+        url : url,
+        data : JSON.stringify(data),
+        headers: { 
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': true 
+        },
         type : "PUT", //POST, PUT, DELETE
         dataType : 'json',
         success: function() {
-            
-            console.log('insertOK');
+            console.log('updateOK');
         },
         error: function(error) {
             console.log('errorInsert -->', error);
         },
         complete : function(xhr, status) {
-            //location.reload();
-            loadData();
+            location.reload();
         }
     })
 }
 
 //Delete the data
-function onDelete(td, id) {
+function categoryDelete(td, id) {
     $.ajax({
-        url : `https://g21b567102d46a9-f8e2c8h6ah6jkxtz.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/estudiantes/estudiantes/${id}`,
+        url : `http://${host}:8080/api/Category/${id}`,
         data : null,
         type : "DELETE", //POST, PUT, DELETE,
         dataType : 'json',
+        headers: {  
+            'Access-Control-Allow-Origin': true
+        },
         success: function(data) {
             console.log('Eliminado -->', data);
+            location.reload();
         },
         error: function(error) {
             alert('Error');
